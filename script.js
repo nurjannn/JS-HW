3b306c8753f8b0daafd34e9a91085a51b4b062bc
Nurjan <nurjan.101110@gmail.com>
2024-07-19 18:02:04 +0300

    HW-17

---
+++ script.js
@@ -0,0 +1,103 @@
+// let a = 10;
+
+// if (a === 10) {
+//   console.log("Верно");
+// } else {
+//   console.log("Неверно");
+// }
+
+// //? 2
+
+// let baatir = 33;
+
+// if (baatir >= 1 && baatir <= 20) {
+//   console.log("birinchi");
+// } else if (baatir >= 21 && baatir <= 48) {
+//   console.log("ekinchi");
+// } else if (baatir >= 49 && baatir <= 90) {
+//   console.log("uchunchu");
+// } else {
+//   console.log("andai kvartira bul jacta jok brat!");
+// }
+
+//? 3
+
+// let a = 3;
+
+// if (a > 0 && a < 5) {
+//     console.log('Верно');
+// } else {
+//     console.log('Неверно');
+// }
+
+//? 4
+
+// const hour = 9;
+
+// if (hour >= 6 && hour < 12) {
+//   console.log("Доброе утро!");
+// } else if (hour >= 12 && hour < 17) {
+//   console.log("Добрый день!");
+// } else {
+//   console.log("Добрый вечер!");
+// }
+
+//? 5
+
+// const hour = 9;
+// const name = "Нуржан";
+
+// let greeting;
+
+// if (hour >= 6 && hour < 12) {
+//   greeting = "Доброе утро, " + name + "!";
+// } else if (hour >= 12 && hour < 17) {
+//   greeting = "Добрый день, " + name + "!";
+// } else {
+//   greeting = "Добрый вечер, " + name + "!";
+// }
+
+// console.log(greeting);
+
+//? 6
+
+// const age = 70;
+
+// if (age < 6 || age >= 65) {
+//   console.log("Скидка");
+// } else {
+//   console.log("Без скидки");
+// }
+
+//? 7
+
+// const player1Choice = prompt(
+// ).toLowerCase();
+// const player2Choice = prompt(
+// ).toLowerCase();
+
+// const validChoices = ["камень", "ножницы", "бумага"];
+
+// if (
+//   !validChoices.includes(player1Choice) ||
+//   !validChoices.includes(player2Choice)
+// ) {
+//   console.log(
+//   );
+// } else {
+//   if (player1Choice === player2Choice) {
+//     console.log("Ничья");
+//   } else if (
+//     (player1Choice === "камень" && player2Choice === "ножницы") ||
+//     (player1Choice === "ножницы" && player2Choice === "бумага") ||
+//     (player1Choice === "бумага" && player2Choice === "камень")
+//   ) {
+//     console.log("выйграл первый");
+//   } else {
+//     console.log("выйнрал второй");
+//   }
+// }
+// //? 9
+// let num = 5;
+
+// num === 5 ? console.log("Five") : console.log("Not five");
